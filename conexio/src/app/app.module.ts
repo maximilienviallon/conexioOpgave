8880594bd1cc3166cde71da9d8ee0d3402e44b69
maximilienviallon <maximilien.viallon@gmail.com>
2021-03-29 16:35:44 +0200

    add table header and button components

--- conexio/src/app/app.module.ts
+++ conexio/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TableHeaderComponent } from './table-header/table-header.component';
+import { ButtonComponent } from './button/button.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TableHeaderComponent,
+    ButtonComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
